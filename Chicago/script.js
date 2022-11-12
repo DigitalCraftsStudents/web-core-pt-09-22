8a960666acab9d05ad50f2a3bac07ad390755f77
Your Name <tan.ledang@digitalcrafts.com>
2022-11-12 10:17:00 -0500

    render img html to page

--- Chicago/script.js
+++ Chicago/script.js
@@ -4,12 +4,17 @@ const exampleSearchBox = "dogs";
 const result =  await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${exampleSearchBox}&fields=id,title,image_id`)
 const parsedResponse = await result.json();
 console.log(parsedResponse)
+const htmlArray = [];
     for (let i = 0; i < parsedResponse.data.length; i++)
     {
         const src = `https://www.artic.edu/iiif/2/${parsedResponse.data[i].image_id}/full/843,/0/default.jpg`
-        console.log(src);
+        const imgHtml = `<img src=${src}></img>`
+        htmlArray.push(imgHtml)
     }
 
+const htmlString = htmlArray.join('');
+console.log(htmlString)
+document.getElementById("imageContainer").innerHTML = htmlString;
 // .then(async (result)=>{
 //     const parsedResponse = await result.json();
 //     console.log(parsedResponse.data.size, parsedResponse.data.length);
