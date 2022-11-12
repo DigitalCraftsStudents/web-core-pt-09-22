4f90d24b48192677716aec046452ccb0aa50c1cb
Your Name <tan.ledang@digitalcrafts.com>
2022-11-12 12:28:28 -0500

    buttons classnames change with page change

--- Chicago/script.js
+++ Chicago/script.js
@@ -1,7 +1,8 @@
+let page = 1;
+
 const fetchAndDisplayImage = async (exampleSearchBox) => { 
 
 const encodedSearchString = encodeURIComponent(exampleSearchBox)
-const page = 1;
 
 const result =  await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${encodedSearchString}&fields=id,title,image_id&page=${page}`)
 const parsedResponse = await result.json();
@@ -24,4 +25,22 @@ searchForm.addEventListener("submit", function(event) {
     const searchString = document.getElementsByClassName("search-bar")[0].value;
     
     fetchAndDisplayImage(searchString);
+})
+
+const changePage = (pageNumber) => {
+    page = pageNumber;
+}
+
+const paginationContainer = document.getElementsByClassName("pagination")[0];
+
+paginationContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("pageNumber")) {
+        const currentActiveButton = document.getElementById(page);
+        currentActiveButton.className = "pageNumber";
+
+        event.target.className = "active pageNumber"
+        
+        page = event.target.id;
+        console.log("new page number is:", page)
+    }
 })
